15840b91a435eb7b435e2bcc25d293dc26eae47e
Gabriel Bernabeu <gabrielbernabeuetude@gmail.com>
2025-04-22 22:43:52 +0900

    [FIX] [RTPhysicsRaycaster]

--- Assets/_Common/Scripts/BasePackage/RTPhysicsRaycaster.cs
+++ Assets/_Common/Scripts/BasePackage/RTPhysicsRaycaster.cs
@@ -4,20 +4,86 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using NaughtyAttributes;
 
-[RequireComponent(typeof(Camera))]
-public class RTPhysicsRaycaster : PhysicsRaycaster
+[RequireComponent(typeof(RawImage))]
+public class RTPhysicsRaycaster : BaseRaycaster
 {
-    [Tooltip("UI RawImage showing the RenderTexture")]
-    public RawImage targetRawImage;
+    #region Copied from PhysicsRaycaster.cs
+
+    /// <summary>
+    /// Const to use for clarity when no event mask is set
+    /// </summary>
+    protected const int kNoEventMaskSet = -1;
+
+    /// <summary>
+    /// Layer mask used to filter events. Always combined with the camera's culling mask if a camera is used.
+    /// </summary>
+    [SerializeField]
+    protected LayerMask m_EventMask = kNoEventMaskSet;
+
+    /// <summary>
+    /// Event mask used to determine which objects will receive events.
+    /// </summary>
+    public int finalEventMask
+    {
+        get { return (eventCamera != null) ? eventCamera.cullingMask & m_EventMask : kNoEventMaskSet; }
+    }
+
+    /// <summary>
+    /// Layer mask used to filter events. Always combined with the camera's culling mask if a camera is used.
+    /// </summary>
+    public LayerMask eventMask
+    {
+        get { return m_EventMask; }
+        set { m_EventMask = value; }
+    }
+
+    protected Camera m_EventCamera;
+
+    #endregion
+
+    [Tooltip("Source camera of the render texture"), SerializeField]
+    private Camera sourceCamera;
     [SerializeField, ReadOnly] private GraphicRaycaster graphicRaycaster;
 
+    private RawImage rawImage;
     //Used for not directly disabling raycaster during Raycast method (throws an error)
     private bool shouldGraphicRaycasterBeEnabled = true;
+
+    public override Camera eventCamera
+    {
+        get
+        {
+            if (m_EventCamera == null)
+            {
+                Canvas lCanvas = GetComponentInParent<Canvas>();
+
+                switch (lCanvas.renderMode)
+                {
+                    case RenderMode.ScreenSpaceOverlay:
+                        m_EventCamera = null;
+                        break;
+                    case RenderMode.ScreenSpaceCamera:
+                        m_EventCamera = lCanvas.worldCamera;
+                        break;
+                    case RenderMode.WorldSpace:
+
+                        if (lCanvas.worldCamera != null)
+                            m_EventCamera = lCanvas.worldCamera;
+                        else
+                            m_EventCamera = Camera.main;
+                        break;
+                }
+            }
+
+            return m_EventCamera;
+        }
+    }
 
     protected override void Start()
     {
         base.Start();
-        graphicRaycaster = targetRawImage.GetComponentInParent<GraphicRaycaster>();
+        rawImage = GetComponent<RawImage>();
+        graphicRaycaster = GetComponentInParent<GraphicRaycaster>();
     }
 
     private void LateUpdate()
@@ -28,10 +94,10 @@ public class RTPhysicsRaycaster : PhysicsRaycaster
 
     public override void Raycast(PointerEventData eventData, List<RaycastResult> resultAppendList)
     {
-        if (targetRawImage == null || targetRawImage.texture == null)
+        if (rawImage.texture == null)
             return;
 
-        RectTransform lRectTransform = targetRawImage.rectTransform;
+        RectTransform lRectTransform = rawImage.rectTransform;
 
         if (!RectTransformUtility.ScreenPointToLocalPointInRectangle(lRectTransform, eventData.position, 
             eventData.pressEventCamera, out Vector2 lLocalPoint))
@@ -52,10 +118,10 @@ public class RTPhysicsRaycaster : PhysicsRaycaster
 
         shouldGraphicRaycasterBeEnabled = false;
 
-        Ray lRay = eventCamera.ViewportPointToRay(new Vector3(lUv.x, lUv.y, 0));
+        Ray lRay = sourceCamera.ViewportPointToRay(new Vector3(lUv.x, lUv.y, 0));
 
         // Use Physics.RaycastAll to get all hits
-        RaycastHit[] lHits = Physics.RaycastAll(lRay, eventCamera.farClipPlane, finalEventMask);
+        RaycastHit[] lHits = Physics.RaycastAll(lRay, sourceCamera.farClipPlane, finalEventMask);
 
         if (lHits.Length > 0)
         {
